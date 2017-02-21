ea04ed648ecd888861cef40aa21e272b9ac2dde3
HaoYu <jinyu121@126.com>
2017-02-21 22:43:29 +0800

    一个简单的前台模板

--- CanteenWebsite/urls.py
+++ CanteenWebsite/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<category_id>[0-9]+)/$', views.category, name='category'),
 ]
--- CanteenWebsite/views.py
+++ CanteenWebsite/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-from django.http import HttpResponse
+from .models import Category
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    context = {}
+    return render(request=request,
+                  template_name="index.html",
+                  context=context
+                  )
+
+
+def category(request, category_id):
+    category_query = get_object_or_404(Category, pk=category_id)
+    context = {
+        "category": category_query
+    }
+    return render(request=request,
+                  template_name="category.html",
+                  context=context
+                  )
