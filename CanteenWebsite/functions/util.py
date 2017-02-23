40371e91a31f2f8a0f5d4d602b579c098798c79f
Yu Hao <jinyu121@haoyu.love>
2017-02-23 20:42:32 +0800

    导入默认设置

--- CanteenWebsite/functions/util.py
+++ CanteenWebsite/functions/util.py
@@ -1,4 +1,6 @@
 import re
+import json
+from CanteenWebsite.models import Setting
 
 DISCOUNT_TYPE = {
     "conditinal": {
@@ -45,3 +47,53 @@ def value_set_select(key, value_set, default, wrapper):
         except:
             pass
     return v
+
+
+def setting_get(name, default=None, create=False):
+    """
+    获取设置
+    :param name: name
+    :param default: name不存在时的默认值
+    :return: value 或 default
+    """
+    try:
+        result = Setting.objects.get(name=name)
+        return result.value
+    except:
+        if create:
+            setting_set(name=name, value=default)
+        return default
+
+
+def setting_set(name, value=""):
+    """
+    写入设置
+    :param name: name
+    :param value: value
+    :return: True，表示成功
+    """
+    try:
+        result = Setting.objects.get(name=name)
+        result.value = value
+        result.save()
+    except:
+        Setting.objects.create(name=name, value=value)
+
+    return True
+
+
+def setting_set_json(name, value=None):
+    try:
+        json_string = json.dumps(value)
+    except:
+        json_string = json.dumps(None)
+    setting_set(name=name, value=json_string)
+    return True
+
+
+def setting_get_json(name, default=None):
+    json_string = setting_get(name=name)
+    try:
+        return json.loads(json_string)
+    except:
+        return default
---
+++ CanteenWebsite/management/commands/setup.py
@@ -0,0 +1,30 @@
+import os
+from django.core.management.base import BaseCommand, CommandError
+from CanteenWebsite.functions.util import setting_set, setting_set_json
+
+
+class Command(BaseCommand):
+    help = "基本设置"
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS("导入默认设置"))
+        # 站点名称
+        setting_set("site_name", "Canteen")
+        # 站点副标题
+        setting_set("site_slogan", "发现好物")
+        # 首页显示什么 random/none
+        setting_set("index_page", "random")
+        # 文件结构
+        setting_set_json("column_index", [0, 1, 2, 4, 5, 6, 8, 9, 12, 13, 17, 18, 19, 20, 21])
+        # 导入前清空
+        setting_set_json("delete_before_import", False)
+        # 分类黑名单
+        setting_set_json("blacklist_category", {"active": False, "list": []})
+        # 分类白名单
+        setting_set_json("whitelist_category", {"active": False, "list": []})
+        # 商品黑名单
+        setting_set_json("blacklist_goods", {"active": False, "list": []})
+        # 商品白名单
+        setting_set_json("whitelist_goods", {"active": False, "list": []})
+
+        self.stdout.write(self.style.SUCCESS("完成"))
