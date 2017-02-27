19e8b740386b7b7c4f14fbef65298a2babe48c3d
HaoYu <jinyu121@126.com>
2017-02-27 09:33:46 +0800

    设置项

--- CanteenWebsite/management/commands/setup.py
+++ CanteenWebsite/management/commands/setup.py
@@ -1,5 +1,5 @@
-import os
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+
 from CanteenWebsite.functions.util import setting_set, setting_set_json
 
 
@@ -27,12 +27,16 @@ class Command(BaseCommand):
         # 导入前清空
         setting_set_json("delete_before_import", False)
         # 分类黑名单
-        setting_set_json("blacklist_category", {"active": False, "list": []})
+        setting_set_json("blacklist_category_active", False)
+        setting_set_json("blacklist_category", [])
         # 分类白名单
-        setting_set_json("whitelist_category", {"active": False, "list": []})
+        setting_set_json("whitelist_category_active", False)
+        setting_set_json("whitelist_category", [])
         # 商品黑名单
-        setting_set_json("blacklist_goods", {"active": False, "list": []})
+        setting_set_json("blacklist_goods_active", False)
+        setting_set_json("blacklist_goods", [])
         # 商品白名单
-        setting_set_json("whitelist_goods", {"active": False, "list": []})
+        setting_set_json("whitelist_goods_active", False)
+        setting_set_json("whitelist_goods", [])
 
         self.stdout.write(self.style.SUCCESS("完成"))
