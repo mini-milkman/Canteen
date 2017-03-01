8c1abf36c07ce9eba3c8fadba72fea8ec32a431e
Yu Hao <jinyu121@126.com>
2017-03-01 22:47:41 +0800

    后台设置页面雏形

--- CanteenWebsite/admin.py
+++ CanteenWebsite/admin.py
@@ -1,10 +1,11 @@
+from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.admin import AdminSite
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
+from .admin_forms import OptionsForm, DataImportOptionForm, DataImportForm
 from .models import Category
 from .models import Goods
 from .models import Setting
-from django.urls import reverse
 
 
 @admin.register(Category)
@@ -36,3 +37,55 @@ class SettingAdmin(admin.ModelAdmin):
             return obj.value
 
     value_show.__name__ = "值"
+
+    def get_urls(self):
+        urls = super(SettingAdmin, self).get_urls()
+        my_urls = [
+            url(r'^general_options/$', self.general_options, name="general_options"),
+            url(r'^data_import_options/$', self.data_import_options, name="data_import_options"),
+            url(r'^data_import/$', self.data_import, name="data_import")
+        ]
+        return my_urls + urls
+
+    def general_form(self, request, ModelForm, form_url="", extra_context=None, title="设置"):
+        if request.method == 'POST':
+            form = ModelForm(request.POST)
+            if form.is_valid():
+                form_validated = True
+                form.save()
+            else:
+                form_validated = False
+        else:
+            form = ModelForm()
+        context = dict(
+            self.admin_site.each_context(request),
+            title=title,
+            form=form,
+            form_url=form_url
+        )
+        context.update(extra_context or {})
+        return TemplateResponse(request,
+                                "admin/canteen_form.html",
+                                context
+                                )
+
+    def general_options(self, request, form_url='', extra_context=None):
+        return self.general_form(request,
+                                 OptionsForm,
+                                 form_url=form_url,
+                                 extra_context=extra_context,
+                                 title="基本设置")
+
+    def data_import_options(self, request, form_url='', extra_context=None):
+        return self.general_form(request,
+                                 DataImportOptionForm,
+                                 form_url=form_url,
+                                 extra_context=extra_context,
+                                 title="数据导入设置")
+
+    def data_import(self, request, form_url='', extra_context=None):
+        return self.general_form(request,
+                                 DataImportForm,
+                                 form_url=form_url,
+                                 extra_context=extra_context,
+                                 title="数据导入")
--- CanteenWebsite/urls.py
+++ CanteenWebsite/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
 
 from . import views
-from CanteenWebsite.admin_views import canteen_admin_site
 
 urlpatterns = [
-    url(r"^admin/", canteen_admin_site.urls),
     url(r'^$', views.index, name='index'),
     url(r'^(?P<category_id>[0-9]+)/$', views.category, name='category'),
     url(r'^search/(?P<key_word>[\S]+)/$', views.search, name='search'),
