6efeca93adab6a63ee12cb0ebf02630ad3d4c8fa
Yu Hao <jinyu121@126.com>
2017-03-13 11:42:16 +0800

    Detail View (So ugly...)

--- CanteenWebsite/urls.py
+++ CanteenWebsite/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<category_id>[0-9]+)/$', views.category, name='category'),
+    url(r'^category/(?P<category_id>[0-9]+)/$', views.category, name='category'),
+    url(r'^detail/(?P<goods_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^search/(?P<key_word>[\S]+)/$', views.search, name='search'),
 ]
--- CanteenWebsite/views.py
+++ CanteenWebsite/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import render
 
 from CanteenWebsite.models import Category, Goods
 from CanteenWebsite.utils.functions import get_goods_list
-from CanteenWebsite.utils.functions import setting_get
+from CanteenWebsite.utils.functions import setting_get, setting_get_json
 
 
 def index(request):
     index_page = setting_get("index_page", "blank")
     goods_list = None
-    list_style_template = "CanteenWebsite/inclusions/show_list_styles/{}.html".format(
-        setting_get("goods_list_style")
+    context_extend = dict(
+        list_style_template="CanteenWebsite/inclusions/show_list_styles/{}.html".format(
+            setting_get("goods_list_style")
+        ),
+        use_goods_detail_view=setting_get_json("use_goods_detail_view")
     )
 
     if index_page != "blank":
@@ -20,9 +23,9 @@ def index(request):
 
     context = {
         "index_page": index_page,
-        "goods_list": goods_list,
-        "list_style_template": list_style_template
+        "goods_list": goods_list
     }
+    context.update(context_extend)
     return render(request=request,
                   template_name="CanteenWebsite/index.html",
                   context=context
@@ -30,6 +33,13 @@ def index(request):
 
 
 def category(request, category_id):
+    context_extend = dict(
+        list_style_template="CanteenWebsite/inclusions/show_list_styles/{}.html".format(
+            setting_get("goods_list_style")
+        ),
+        use_goods_detail_view=setting_get_json("use_goods_detail_view")
+    )
+
     # 当前分类
     current_category = get_object_or_404(Category, pk=category_id)
 
@@ -37,33 +47,44 @@ def category(request, category_id):
     goods_list_all = current_category.goods_set.all()
     goods_list = get_goods_list(goods_list_all, request)
 
-    # 显示样式
-    list_style_template = "CanteenWebsite/inclusions/show_list_styles/{}.html".format(
-        setting_get("goods_list_style")
-    )
-
     context = {
         "current_category": current_category,
-        "goods_list": goods_list,
-        "list_style_template": list_style_template
+        "goods_list": goods_list
     }
+    context.update(context_extend)
     return render(request=request,
                   template_name="CanteenWebsite/category.html",
                   context=context
                   )
 
 
+def detail(request, goods_id):
+    # 当前商品
+    current_goods = get_object_or_404(Goods, pk=goods_id)
+
+    context = {
+        "current_goods": current_goods
+    }
+    return render(request=request,
+                  template_name="CanteenWebsite/detail.html",
+                  context=context
+                  )
+
+
 def search(request, key_word):
+    context_extend = dict(
+        list_style_template="CanteenWebsite/inclusions/show_list_styles/{}.html".format(
+            setting_get("goods_list_style")
+        ),
+        use_goods_detail_view=setting_get_json("use_goods_detail_view")
+    )
     goods_list_all = Goods.objects.filter(name__contains=key_word)
     goods_list = get_goods_list(goods_list_all, request)
-    list_style_template = "CanteenWebsite/inclusions/show_list_styles/{}.html".format(
-        setting_get("goods_list_style")
-    )
     context = {
         "keyword": key_word,
-        "goods_list": goods_list,
-        "list_style_template": list_style_template
+        "goods_list": goods_list
     }
+    context.update(context_extend)
     return render(request=request,
                   template_name="CanteenWebsite/search.html",
                   context=context
